ee1a4e7da7b614ece3d2e042e661ec21d9696754
zaibshaikh <szaib.shaikh@gmail.com>
2025-02-08 22:10:56 +0530

    c

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const codeInput = document.getElementById('code');
 const submitBtn = document.getElementById('submit-btn');
 const resultDiv = document.getElementById('result');
 
-const secretCode = '21101'; // Replace with your secret code
+const secretCode = '211101'; // Replace with your secret code
 const nextPageUrl = 'main.html'; // Replace with the URL of the new page
 
 form.addEventListener('submit', (e) => {
